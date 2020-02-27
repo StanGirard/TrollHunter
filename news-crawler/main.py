df40748c31fabd7cb1e8fbce0a2468b2a2053c58
StanGirard <girard.stanislas@gmail.com>
2020-02-27 23:00:56 +0100

    [NEWS][CRAWLER] XML key generator

--- news-crawler/main.py
+++ news-crawler/main.py
@@ -1,7 +1,12 @@
 import requests
 import xml.etree.ElementTree as ET
 r = requests.get('https://www.washingtonpost.com/arcio/news-sitemap/')
-print(r.content)
+#print(r.content)
+xmlDict = {}
 root = ET.fromstring(r.content)
 for child in root.iter('*'):
     print(child.tag)
+for sitemap in root:
+    children = sitemap.getchildren()
+    xmlDict[children[0].text] = children[1].text
+print (xmlDict)
