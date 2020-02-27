3e0a483bc3741cf50081b23d16fd47ca4eca56b0
StanGirard <girard.stanislas@gmail.com>
2020-02-27 16:56:57 +0100

    [NEWS][CRAWLER] Sitemap parsin with xml.etree

--- news-crawler/main.py
+++ news-crawler/main.py
@@ -1,5 +1,7 @@
-from usp.tree import sitemap_tree_for_homepage
-
-tree = sitemap_tree_for_homepage('https://www.nytimes.com/')
-for page in tree.all_pages():
-    print(page)
+import requests
+import xml.etree.ElementTree as ET
+r = requests.get('https://www.washingtonpost.com/arcio/news-sitemap/')
+print(r.content)
+root = ET.fromstring(r.content)
+for child in root.iter('*'):
+    print(child.tag)
