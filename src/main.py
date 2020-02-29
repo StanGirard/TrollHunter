1e521d2e8a5b3692e70a172b1e0ece33083e05e8
Quentin Derosin <quentin.derosin@epita.fr>
2020-02-29 17:36:45 +0100

    [FEATURES] - get tweet from search word

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 
-from src.twint_api.request import get_user_tweet
+from src.twint_api.request import get_tweet_from_user, get_tweet_from_search
 
 app = Flask(__name__)
 api = Api(app)
 
 @app.route('/tweets/<string:user>', methods=['GET'])
 def user_tweet(user):
+    return get_tweet_from_user(user,request.args)
 
-    return get_user_tweet(user,request.args)
-
-    # return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
-# @app.route('/tweets/')
+@app.route('/tweets/', methods=['GET'])
+def search_tweet():
+    return  get_tweet_from_search(request.args)
 
 
 if __name__ == '__main__':
--- src/twint_api/request.py
+++ src/twint_api/request.py
@@ -2,30 +2,53 @@ import twint
 import datetime
 import dateutil
 
+
 config = twint.Config()
 
-def get_user_tweet(user,args):
+def get_list_tweets(config,args):
+
+    get_twint_config(config,args)
+    twint.output.tweets_list.clear()
+    twint.run.Search(config)
+    return twint.output.tweets_list
+
+def get_tweet_from_user(user,args):
     config.Username = user
+    config.Search = None
+    get_twint_config(config,args)
+    tweets_result = get_list_tweets(config,args)
+
+    return format_tweet_to_html(tweets_result,user)
+
+def get_tweet_from_search(args):
+    config.Username = None
+    if not "search" in args:
+        return " bad request"
+    config.Search = args["search"]
+    tweet_result = get_list_tweets(config,args)
+    return format_tweet_to_html(tweet_result,"test")
+
+
+
+def get_twint_config(config,args):
     limit = 100
-    since =  datetime.date.today() - datetime.timedelta(days=10)
+    since = datetime.date.today() - datetime.timedelta(days=10)
     since = since.isoformat()
     retweet = False
+
     if "limits" in args:
         limit = args["limits"]
     if "since" in args:
         since = args["since"]
     if "retweet" in args:
         retweet = args["retweet"]
+    if "search" in args:
+        config.Search = args["search"]
+
     config.Limit = limit
     config.Retweets = retweet
     config.Since = since
-    config.Store_object_tweets_list = True
     config.Store_object = True
-
-    twint.run.Search(config)
-    tweets_result = twint.output.tweets_list
-    return format_tweet_to_html(tweets_result,user)
-
 
 def format_tweet_to_html(tweets_list,word):
     ret = "<h1>tweet from {} </h1><br>".format(word)
