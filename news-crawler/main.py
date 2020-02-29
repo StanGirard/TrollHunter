2b6dd7bbe62d0c4eb0c5bfd0822143df64107d2f
StanGirard <girard.stanislas@gmail.com>
2020-02-29 18:00:13 +0100

    [NEWS] Extraction key from sitemap

--- news-crawler/main.py
+++ news-crawler/main.py
@@ -1,12 +1,13 @@
 import requests
 import xml.etree.ElementTree as ET
-r = requests.get('https://www.washingtonpost.com/arcio/news-sitemap/')
-#print(r.content)
-xmlDict = {}
-root = ET.fromstring(r.content)
-for child in root.iter('*'):
-    print(child.tag)
-for sitemap in root:
-    children = sitemap.getchildren()
-    xmlDict[children[0].text] = children[1].text
-print (xmlDict)
+from sitemap import parse_sitemap
+
+
+print(parse_sitemap("https://primates.dev/sitemap-posts.xml"))
+
+
+
+
+
+
+
--- news-crawler/sitemap.py
+++ news-crawler/sitemap.py
@@ -1,12 +1,31 @@
 import requests
 import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup as Soup
 
-class Sitemap:
-    def __init__(self, url):
-        self.url = url
-        self.xmlDict = []
-        
-    def request_sitemap(self):
-        raw_content = requests.get(self.url)
-        self.root_xml = ET.fromstring(raw_content)
-        return self.root_xml
+def parse_sitemap( url):
+    resp = requests.get(url)
+    # we didn't get a valid response, bail
+    if (200 != resp.status_code):
+        return False
+
+    # BeautifulSoup to parse the document
+    soup = Soup(resp.content)
+
+    # find all the <url> tags in the document
+    urls = soup.findAll('url')
+
+    # no urls? bail
+    if not urls:
+        return False
+
+    # storage for later...
+    out = []
+
+    # extract what we need from the url
+    for u in urls:
+        loc = u.find('loc').string
+        #prio = u.find('priority').string
+        #change = u.find('changefreq').string
+        last = u.find('lastmod').string
+        out.append({'url': loc,'date': last})
+    return out
