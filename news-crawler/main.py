3850089a86cc16db567fe9085b757f9eb3bacb0e
StanGirard <girard.stanislas@gmail.com>
2020-03-01 15:05:50 +0100

    [NEWS][EXTRACTOR] Init

--- news-crawler/main.py
+++ news-crawler/main.py
@@ -1,11 +1,10 @@
 import requests
 import xml.etree.ElementTree as ET
 from sitemap import parse_sitemap
-
+from extractor import extract_text_from_url
 
 print(parse_sitemap("https://primates.dev/sitemap-posts.xml", ["loc", "lastmod"]))
-
-
+print(extract_text_from_url("https://primates.dev"))
 
 
 
