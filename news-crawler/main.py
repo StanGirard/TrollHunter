44d15a0080aa19e9bc0e4fa48c9c4e0a21c19c5e
StanGirard <girard.stanislas@gmail.com>
2020-03-02 14:17:41 +0100

    [NEWS[CRAWLER] Recursive

--- news-crawler/main.py
+++ news-crawler/main.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 from sitemap import parse_sitemap
 from extractor import extract_text_from_url
 
-print(parse_sitemap("https://primates.dev/sitemap-posts.xml", ["loc", "lastmod"]))
+print(parse_sitemap("https://primates.dev/sitemap.xml", ["loc", "lastmod"]))
 #print(extract_text_from_url("https://primates.dev"))
 
 
