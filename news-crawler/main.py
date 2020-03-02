96a150b2c19169d03328831c0953c7c40a582467
StanGirard <girard.stanislas@gmail.com>
2020-03-02 14:50:06 +0100

    [NEWS][CRAWLER] Extract specific key from sitemap

--- news-crawler/main.py
+++ news-crawler/main.py
@@ -2,9 +2,12 @@ import requests
 import xml.etree.ElementTree as ET
 from sitemap import parse_sitemap
 from extractor import extract_text_from_url
+import pandas as pd
 
-print(parse_sitemap("https://primates.dev/sitemap.xml", ["loc", "lastmod"]))
+dataframe = parse_sitemap("https://www.washingtonpost.com/arcio/news-sitemap/", ["loc", "lastmod","news:title" ,"news:keywords"])
 #print(extract_text_from_url("https://primates.dev"))
+print(dataframe)
+#dataframe.to_csv(r'./data.csv')
 
 
 
