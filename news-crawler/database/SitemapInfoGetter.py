c311299955b38f397eda82838463a751b702b523
Dominique Besson <guydombess@gmail.com>
2020-03-03 10:20:52 +0100

    [DATABASE][UPDATE] missing title fetching

---
+++ news-crawler/database/SitemapInfoGetter.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append("../")
+from sitemap import parse_sitemap
+import time
+
+class SitemapInfo:
+    def __init__(self, baseUrl):
+        self._baseUrl = baseUrl
+        self._pdResult = self._retriveUrlsByMap(self._baseUrl)
+        if self._pdResult is not False:
+            self._fetchAllUrlInfo()
+
+    def _retriveUrlsByMap(self, map):
+        result = parse_sitemap(map, ["loc", "lastmod","image:loc", "news:keywords" ])
+        # for each urls, get info
+        #print(result)
+        return result
+
+    def _fetchAllUrlInfo(self):
+        for index, row in self._pdResult.iterrows():
+            date = row[2].split(".")[0]
+            timestamp = 0
+            if date != "None":
+                timestamp = time.mktime(time.strptime(date, '%Y-%m-%dT%H:%M:%S'))
+
+            row[2] = timestamp
+
+        print(self._pdResult)
--- news-crawler/database/update_database.py
+++ news-crawler/database/update_database.py
@@ -1,11 +1,20 @@
 import sqlite3
+import sys
 
-conn = sqlite3.connect('trollhunter.db')
-print ("Opened database successfully")
+sys.path.append("../")
+from SitemapInfoGetter import SitemapInfo
 
+# connect to the local DB
+conn = sqlite3.connect('./trollhunter.db')
+print("Opened database successfully")
+
+# select all providers
 cursor = conn.execute("SELECT id, url_provider, site_map_path from NEWS_PROVIDERS")
+
 for row in cursor:
-    parse_sitemap(row[1]+row[2], ["loc", "lastmod"])
+    site_map_url = row[1]+row[2]
+    sitemapGetter = SitemapInfo(site_map_url)
+
 
 print("Operation done successfully")
 conn.close()
