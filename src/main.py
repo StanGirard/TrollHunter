e9369e5bf3c43c23d42db860158b619f2f9a8f0b
Pierre-Jean Liard <pierrejean.liard@gmail.com>
2020-03-05 00:49:42 +0100

    Main : modification test search

--- src/main.py
+++ src/main.py
@@ -1,25 +1,30 @@
 from flask import Flask, request
 from multiprocessing.dummy import Pool, Manager
 
-from src.twint_api.request import get_tweet_from_user, get_tweet_from_search, get_info_from_user
+from src.twint_api.request import get_tweet_from_user, get_tweet_from_search, get_info_from_user, get_origin_tweet
 
 app = Flask(__name__)
 # api = Api(app)
 pool = Pool(processes=15)
 manager = Manager()
 queue_data = manager.Queue()
+
+
 @app.route('/tweets/<string:user>', methods=['GET'])
 def user_tweet(user):
+    return get_tweet_from_user(user, request.args)
 
-    return get_tweet_from_user(user,request.args)
 
 @app.route('/tweets/', methods=['GET'])
 def search_tweet():
-    return  get_tweet_from_search(request.args)
+    return get_origin_tweet(request.args)
+#    return get_tweet_from_search(request.args)
 
-@app.route('/user/info/<string:user>',methods=['GET'])
+
+@app.route('/user/info/<string:user>', methods=['GET'])
 def user_info(user):
-    return get_info_from_user(user,request.args)
+    return get_info_from_user(user, request.args)
+
 
 if __name__ == '__main__':
     app.run()
