c10d30fead0cda1cbe14d97e9482eb88a427681f
StanGirard <girard.stanislas@gmail.com>
2020-03-10 17:23:49 +0100

    [PIP] Bumped Version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='TrollHunter',
 
-    version='0.2.1',
+    version='0.2.2',
     description='TrollHunter',
     url="https://github.com/StanGirard/TrollHunter",
     license="GPL",
