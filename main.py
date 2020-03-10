b888d3fd5d6bc56e3ae60a633e525cebe2dc03bb
StanGirard <girard.stanislas@gmail.com>
2020-03-10 23:19:01 +0100

    [REFACTOR] Include in init

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from TrollHunter.news_crawler.sitemap import elastic_sitemap
-from TrollHunter.news_crawler.database import get_all_sitemap
+from TrollHunter.news_crawler import elastic_sitemap
+from TrollHunter.news_crawler import get_all_sitemap
 from TrollHunter import Gorafi
 
 db_sitemap = {x[0]: x for x in get_all_sitemap()}
