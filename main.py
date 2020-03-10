a835969ca159d647ac0ea718f9698e8bdaaecf89
StanGirard <girard.stanislas@gmail.com>
2020-03-10 19:35:53 +0100

    [GORAFI] Refactor

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from TrollHunter.Gorafi.sitemap.data_elastic import elastic_sitemap
+from TrollHunter.Gorafi.database.postgres_database import get_all_sitemap
 
+db_sitemap = {x[0]: x for x in get_all_sitemap()}
 
-
-elastic_sitemap("https://www.lemonde.fr/sitemap_news.xml",["loc", "lastmod", "news:keywords"], sort = "loc", influxdb = True)
+elastic_sitemap("https://www.lemonde.fr/sitemap_news.xml",["loc", "lastmod", "news:keywords"], db_sitemap, sort = "loc", influxdb = True)
 
 
 
