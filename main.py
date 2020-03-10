486af2fbf67d9d599e9e5d6f30353048c296a7f7
StanGirard <girard.stanislas@gmail.com>
2020-03-10 20:14:23 +0100

    [GORAFI] Class

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from TrollHunter.Gorafi.sitemap.data_elastic import elastic_sitemap
-from TrollHunter.Gorafi.database.postgres_database import get_all_sitemap
+from TrollHunter.news_crawler.sitemap.data_elastic import elastic_sitemap
+from TrollHunter.news_crawler.database.postgres_database import get_all_sitemap
+from TrollHunter import Gorafi
 
 db_sitemap = {x[0]: x for x in get_all_sitemap()}
-
-elastic_sitemap("https://www.lemonde.fr/sitemap_news.xml",["loc", "lastmod", "news:keywords"], db_sitemap, sort = "loc", influxdb = True)
+hello = Gorafi()
+hello.sitemap("https://www.lemonde.fr/sitemap_news.xml")
+#elastic_sitemap("https://www.lemonde.fr/sitemap_news.xml",["loc", "lastmod", "news:keywords"], db_sitemap, sort = "loc", influxdb = True)
 
 
 
