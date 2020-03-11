a5dd46aaa451c5b2462e64c7178eae5fbf55346e
Pierre-Jean Liard <pierrejean.liard@gmail.com>
2020-03-11 18:11:56 +0100

    Ajout de la fonction pour avoir la polarity d'une liste de tweets

--- TrollHunter/texto/Sentiment.py
+++ TrollHunter/texto/Sentiment.py
@@ -1,12 +1,13 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from textblob import TextBlob
 import pandas as pd
 
 
-def get_sentiment_from_tweets(tweets):
+def get_sentiment_from_tweets(tweet):
     analyser = SentimentIntensityAnalyzer()
     sentiment_scores = []
 
-    for t in tweets:
+    for t in tweet:
         score = analyser.polarity_scores(t)
         score['tweet'] = t
         sentiment_scores.append(score)
@@ -16,8 +17,24 @@ def get_sentiment_from_tweets(tweets):
     return sentiment_df
 
 
+def get_polarity(tweet):
+    tab_result = {'tweet': [],
+                  'polarity': []}
+
+    for t in tweet:
+        obj = TextBlob(t)
+        polarity = obj.sentiment.polarity
+
+        tab_result['tweet'].append(t)
+        tab_result['polarity'].append(polarity)
+
+    df_result = pd.DataFrame(tab_result, columns=['tweet', 'polarity'])
+
+    return df_result
+
+
 if __name__ == '__main__':
     tweets = ["I'm so happy", "I surprised him", "It's a sad news", "I'm happy and sad at the same time"]
-    result = get_sentiment_from_tweets(tweets)
+    result = get_polarity(tweets)
 
     print(result)
