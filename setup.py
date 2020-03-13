9f92bb722ed2b81655a6d4673d0f98251ffa15aa
Stan Girard <girard.stanislas@gmail.com>
2020-03-13 12:08:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='TrollHunter',
 
-    version='0.2.7',
+    version='0.2.8',
     description='TrollHunter',
     url="https://github.com/StanGirard/TrollHunter",
     long_description=long_description,
