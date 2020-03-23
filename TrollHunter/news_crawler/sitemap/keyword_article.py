19da34ceac2af85bb22408f88da4a4fb6d01409d
Mathis <mathis.coquet@epita.fr>
2020-03-23 14:38:32 +0100

    [NEWS][ES] extract keywords from article

--- TrollHunter/news_crawler/sitemap/keyword_article.py
+++ TrollHunter/news_crawler/sitemap/keyword_article.py
@@ -8,34 +8,23 @@ def get_null_keywords_es(es, size):
 
 
 def set_keywords(hit):
-    # call texto
-    keyword = ''
     article = Article(hit['_id'])
     article.download()
     article.parse()
     article.nlp()
-    print(article.keywords)
-    hit['_source']['news:keywords'] = keyword
+    hit['_source']['news:keywords'] = ",".join(article.keywords)
     return hit
 
 
 def update_keyword_es(es, hit):
     es.update(index='sitemaps',
               id=hit['_id'],
-              body={
-                  "script": {
-                      "source": "ctx._source.news:keywords = params.keywords",
-                      "lang": "painless",
-                      "params": {"keywords": hit['_source']['news:keywords']},
-                  }
-              })
+              body={"doc": {"news:keywords": hit['_source']['news:keywords']}})
 
 
-def define_keywords_article(size=100, host = "142.93.170.234", port = 9200, user="elastic", password="changeme"):
+def define_keywords_article(size=100, host="142.93.170.234", port=9200, user="elastic", password="changeme"):
     es = Elasticsearch(hosts=[{'host': host, 'port': port}], http_auth=(user, password))
     for hit in get_null_keywords_es(es, size):
         hit = set_keywords(hit)
-        # update_keyword_es(es, hit)
+        update_keyword_es(es, hit)
 
-
-define_keywords_article(1)
